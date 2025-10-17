58e43c7fa9a8f8b5b40e24acd2fa25ae4e01e459
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-17 21:24:30 +0900

    Remove userInfo prop from Header component

--- app/materials/page.tsx
+++ app/materials/page.tsx
@@ -247,7 +247,7 @@ const MaterialsPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header userInfo={userInfo} />
+      <Header />
 
       <div className="max-w-7xl mx-auto px-6 py-8">
         {/* 헤더 */}
